885cc84edede391b4ba868cc290547272b6d9302
KobayashiComputing <andy@kobayashicomputing.com>
2025-02-03 13:14:19 -0500

    Added 'constants.py' and updated '.gitignore'.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from constants import *
 
 def main():
+    initRestuls = pygame.init()
     print("Starting asteroids!")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags = pygame.SCALED)
+        screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill("black")
+        pygame.display.flip()
+
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
