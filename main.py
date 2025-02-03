52000f4af49e9c3c722f0a719cb711cf1d68a919
KobayashiComputing <andy@kobayashicomputing.com>
2025-02-03 13:24:57 -0500

    Added 'circleshape.py'.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     initRestuls = pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     while True:
         for event in pygame.event.get():
@@ -12,6 +14,10 @@ def main():
         screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
         screen.fill("black")
         pygame.display.flip()
+
+
+        dt = clock.tick(60)/1000
+
 
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
